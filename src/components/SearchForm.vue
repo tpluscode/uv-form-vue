0c1decba5185f4fc9d9257b519f84ca01d9ab1eb
Martin Maillard <self@martin-maillard.com>
2020-04-22 17:50:08 +0200

    Add search modal

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form class="modal-card">
+    <header class="modal-card-head">
+      <h2 class="modal-card-title">Search</h2>
+    </header>
+    <section class="modal-card-body">
+      <b-field label="Terms">
+        <b-input required ref="terms" />
+      </b-field>
+      <b-field>
+        <b-button type="is-primary" icon-left="search">Search</b-button>
+      </b-field>
+    </section>
+  </form>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.$refs.terms.focus()
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,66 @@
 <template>
   <div class="section">
-    <h1 class="title">Form demo</h1>
+    <div class="level">
+      <div class="level-item-left">
+        <h1 class="title">Docuteam curator demo</h1>
+      </div>
+      <div class="level-item-right">
+        <b-button
+          type="is-white"
+          icon-left="search"
+          @click="search"
+          v-shortkey="['ctrl', 'f']"
+          @shortkey="search"
+        >
+          Search
+        </b-button>
+      </div>
+    </div>
 
     <b-tabs v-model="activeTab" :animated="false">
       <b-tab-item v-for="step in steps" :key="step.id" :label="step.name">
         <Form :description="step" />
       </b-tab-item>
     </b-tabs>
+
+    <b-modal
+      :active.sync="showSearch"
+      has-modal-card
+      trap-focus
+      aria-role="dialog"
+      aria-modal>
+      <SearchForm />
+    </b-modal>
   </div>
 </template>
 
 <script>
 import Form from '@/components/Form.vue'
+import SearchForm from '@/components/SearchForm.vue'
 
 export default {
   name: 'Home',
 
   components: {
-    Form
+    Form,
+    SearchForm
   },
 
   data: () => ({
     activeTab: 0,
-    steps: []
+    steps: [],
+    showSearch: false
   }),
 
   async mounted () {
     const response = await fetch('forms.json')
     this.steps = await response.json()
+  },
+
+  methods: {
+    search () {
+      this.showSearch = true
+    }
   }
 }
 </script>
