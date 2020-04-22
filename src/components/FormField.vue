4cca1d76cb7389164c0b40bbab22a9145c63b330
Martin Maillard <self@martin-maillard.com>
2020-04-22 18:09:15 +0200

    Add more fields

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="field">
     <label v-if="fieldLabel" :for="fieldId" class="label">{{ fieldLabel }}</label>
-    <input :id="fieldId" :type="fieldType" :value="fieldValue" class="input" />
+
+    <div class="control">
+      <div v-if="fieldType === 'select'" class="select">
+        <b-select :id="fieldId" :value="fieldValue">
+          <option></option>
+          <option v-for="(choice, index) in choices" :key="index" :value="choice.value">
+            {{ choice.label }}
+          </option>
+        </b-select>
+      </div>
+      <div v-else-if="fieldType === 'checkbox'">
+        <div class="checkbox">
+          <input :id="fieldId" :type="fieldType" :selected="fieldValue" />
+        </div>
+      </div>
+      <div v-else-if="fieldType === 'text'">
+        <textarea class="textarea" :id="fieldId" :value="fieldValue" />
+      </div>
+      <input v-else :id="fieldId" :type="fieldType" :value="fieldValue" class="input" />
+    </div>
   </div>
 </template>
 
@@ -18,7 +37,8 @@ export default {
       fieldType: this.description.type || 'text',
       fieldLabel: this.description.label || '',
       fieldId: this.description.id,
-      fieldValue: ''
+      fieldValue: '',
+      choices: this.description.choices || []
     }
   }
 }
